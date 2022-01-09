ea3489212dbfa36e296a192a80ba2c4a3cff44b2
Nathan <nathandyao@gmail.com>
2022-01-09 06:03:49 +0000

    finish the cowtour. We will need cpp now

--- training_2.4_3_cowtour/cowtour.cpp
+++ training_2.4_3_cowtour/cowtour.cpp
@@ -1,4 +1,12 @@
+/*
+ID: nathand6
+PROG: cowtour
+LANG: C++11
+*/
+
 #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 using ld = long double;
@@ -10,36 +18,59 @@ string s;
 ld adj[MAXN][MAXN];
 ld maxdist[MAXN];
 ld inf = 1e9;
-ls dist(int i, int j){
+ld dist(int i, int j){
   ld dx = x[i] - x[j];
   ld dy = y[i] - y[j];
   return sqrt(dx*dx + dy*dy);
 }
 
 int main(){
-  freoprn("cowtour.in", "r", stdin);
-  froopen("cowtour.out", "r", stdout);
+  string root_path="./training_2.4_3_cowtour/";
+  string fullpath = root_path + "cowtour.in";
+  // freopen("./training_2.4_3_cowtour/cowtour.in", "r", stdin);
+  // freopen("./training_2.4_3_cowtour/cowtour.out", "w", stdout);
+  freopen("cowtour.in", "r", stdin);
+  freopen("cowtour.out", "w", stdout);
   cin >> n;
+
   for(int i=0; i<n; i++){
     cin >> x[i] >> y[i];
   }
 
   for(int i=0; i<n; i++){
     cin >> s;
-    for(int j=0; j<n, j++){
+    for(int j=0; j<n; j++){
       if(s[j]=='1') adj[i][j] = dist(i,j);
       else adj[i][j] = inf;
       if(i==j) adj[i][j] = 0;
     }
   }
 
-  for(int k=0;k<n;k++)
-  for(int i=0;i<n;i++)
-  for(int j=0;j<n;j++)
-    dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
-  
-  for(int i=0;i<n; i++){
-    maxdist[i] = -1;
-    for(int j=0;j<n;j++)  maxdist[i]=max(maxdist[i],dist[i][j]);
+  for(int k=0;k<n;k++){
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        adj[i][j]=min(adj[i][j], adj[i][k]+adj[k][j]);
+      }
+    }
   }
+
+
+
+  ld ans = inf;
+  
+  for(int i=0; i<n; i++){
+    maxdist[i] = -1;
+    for(int j=0;j<n;j++){
+      if(adj[i][j] == inf) continue;
+      maxdist[i]=max(maxdist[i],adj[i][j]);
+    }  
+    for(int j=0;j<i;j++){
+      if(adj[i][j] != inf) continue;
+      ans = min(ans, maxdist[i] + dist(i,j) + maxdist[j]);
+    }
+  }
+
+  for(int i=0;i<n;i++)
+    ans = max(ans, maxdist[i]);
+  cout << fixed << setprecision(6) << ans << '\n';
 }
